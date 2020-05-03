02a81f32cc8f53208be81994673233e084ab4552
Donatien Criaud <30463908+DonaCrio@users.noreply.github.com>
2020-05-04 00:46:33 +0200

    Connect to crawler (#2)
    * first connection
    
    * Add env variable for better control

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-use serde::{Deserialize, Serialize};
-use std::convert::Infallible;
-use warp::Filter;
+mod keeper;
 
-#[derive(Deserialize, Serialize, Debug)]
-struct Node {
-    value: String,
-    parent: String,
-}
-
-async fn persist_node(node: Node) -> Result<impl warp::Reply, Infallible> {
-    println!("{:#?}", node);
-    Ok(warp::reply())
-}
+use dotenv::dotenv;
+use keeper::{Keeper, KeeperConfig};
+use std::env;
+use std::error::Error;
+use std::process;
 
 #[tokio::main]
 async fn main() {
-    let routes = warp::post()
-        .and(warp::any())
-        .and(warp::body::json())
-        .and_then(persist_node);
+    dotenv().ok();
 
-    println!("Server listening on 127.0.0.1:3030");
-    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+    let config = get_config().unwrap_or_else(|err| {
+        eprintln!("Problem initializing crawler config: {}", err);
+        process::exit(1);
+    });
+
+    let keeper = Keeper::new(config);
+    keeper.run().await;
+}
+
+fn get_config() -> Result<KeeperConfig, Box<dyn Error>> {
+    let server_port = env::var("SERVER_PORT")?;
+    let server_port = server_port.parse::<u16>()?;
+
+    let config = KeeperConfig::new(server_port);
+    Ok(config)
 }
