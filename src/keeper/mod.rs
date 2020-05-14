15c501b91b6a7cda27ddf84eac506a6880a02b83
donacrio <donatien.criaud@gmail.com>
2020-05-14 15:09:50 +0200

    reorganise module

---
+++ src/keeper/mod.rs
@@ -0,0 +1,62 @@
+mod persistence;
+mod routing;
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::Server;
+use persistence::database::Database;
+use std::net::SocketAddr;
+
+pub struct KeeperConfig {
+    server_port: u16,
+    db_url: String,
+}
+
+impl KeeperConfig {
+    pub fn new(server_port: u16, db_url: String) -> KeeperConfig {
+        KeeperConfig {
+            server_port,
+            db_url,
+        }
+    }
+}
+
+pub struct Keeper {
+    config: KeeperConfig,
+}
+
+impl Keeper {
+    pub fn new(config: KeeperConfig) -> Keeper {
+        Keeper { config }
+    }
+
+    pub async fn run(&self) {
+        let addr = SocketAddr::from(([0, 0, 0, 0], self.config.server_port));
+        let database = Database::new(&self.config.db_url);
+        let make_service = make_service_fn(move |_| {
+            let database = database.clone();
+            async move {
+                Ok::<_, hyper::Error>(service_fn(move |req| {
+                    let database = database.clone();
+                    async move { routing::routes(req, &database).await }
+                }))
+            }
+        });
+
+        let server = Server::bind(&addr).serve(make_service);
+
+        // And now add a graceful shutdown signal...
+        let shutdown = server.with_graceful_shutdown(shutdown_signal());
+
+        // Run this server for... forever!
+        if let Err(e) = shutdown.await {
+            eprintln!("server error: {}", e);
+        }
+    }
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,60 +3,13 @@ extern crate openssl;
 #[macro_use]
 extern crate diesel;
 
-mod persistence;
-mod routing;
+mod keeper;
 
 use dotenv::dotenv;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::Server;
-use persistence::database::Database;
+use keeper::{Keeper, KeeperConfig};
 use std::env;
 use std::error::Error;
-use std::net::SocketAddr;
 use std::process;
-
-struct KeeperConfig {
-    server_port: u16,
-    db_url: String,
-}
-
-impl KeeperConfig {
-    pub fn new(server_port: u16, db_url: String) -> KeeperConfig {
-        KeeperConfig {
-            server_port,
-            db_url,
-        }
-    }
-}
-
-async fn run(config: &KeeperConfig) {
-    let addr = SocketAddr::from(([0, 0, 0, 0], config.server_port));
-    let database = Database::new(&config.db_url);
-    let make_service = make_service_fn(move |_| {
-        let database = database.clone();
-        async move {
-            Ok::<_, hyper::Error>(service_fn(move |req| {
-                let database = database.clone();
-                async move { routing::routes(req, &database).await }
-            }))
-        }
-    });
-
-    let server = Server::bind(&addr).serve(make_service);
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
-}
-
-fn get_config() -> Result<KeeperConfig, Box<dyn Error>> {
-    let server_port = env::var("SERVER_PORT")?;
-    let server_port = server_port.parse::<u16>()?;
-    let db_url = env::var("DATABASE_URL")?;
-
-    let config = KeeperConfig::new(server_port, db_url);
-    Ok(config)
-}
 
 #[tokio::main]
 async fn main() {
@@ -67,5 +20,15 @@ async fn main() {
         process::exit(1);
     });
 
-    run(&config).await;
+    let keeper = Keeper::new(config);
+    keeper.run().await;
+}
+
+fn get_config() -> Result<KeeperConfig, Box<dyn Error>> {
+    let server_port = env::var("SERVER_PORT")?;
+    let server_port = server_port.parse::<u16>()?;
+    let db_url = env::var("DATABASE_URL")?;
+
+    let config = KeeperConfig::new(server_port, db_url);
+    Ok(config)
 }
