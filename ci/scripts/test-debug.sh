afd32a8a7a6ac435f4ad47322c994983d2f1fe7e
donacrio <donatien.criaud@gmail.com>
2020-06-01 22:17:11 +0200

    fix ci

--- ci/scripts/test-debug.sh
+++ ci/scripts/test-debug.sh
@@ -2,7 +2,7 @@
 
 if [ $TARGET == "x86_64-unknown-linux-musl" ]
 then
-  .ci/scripts/test-musl-debug.sh "$1"
+  ./ci/scripts/test-musl-debug.sh "$1"
 else
   cargo test --target ${TARGET} --verbose
 fi
