21f018eee1ad0b0b28b25c48a55228cc6c21b704
Donatien Criaud <30463908+DonaCrio@users.noreply.github.com>
2020-06-22 17:02:56 +0200

    Add blocking nats subsccriber (#15)
    * add blocking nats subsccriber
    
    * update docker-compose
    
    * fix docker-compose

--- src/nats.rs
+++ src/nats.rs
@@ -11,7 +11,7 @@ impl NatsSubscriber {
         Ok(NatsSubscriber { conn, sub })
     }
     pub fn get_next_message(&self) -> Option<nats::Message> {
-        self.sub.try_next()
+        self.sub.next()
     }
     pub fn close(self) {
         self.conn.close()
